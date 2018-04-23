c04e58c1959049ffc144d1449527e944f2726539
Iñigo Tamayo Uria <itamayo@vicomtech.org>
2018-04-23 16:26:27 +0200

    test change

--- src/contextMenu.component.ts
+++ src/contextMenu.component.ts
@@ -76,7 +76,7 @@ export interface MouseLocation {
             (click)="execute(link, $event); $event.preventDefault(); $event.stopPropagation();"
             innerHTML="{{link.html ? link.html(item) : ''}}"></a>
             <ul class="dropdown-content">
-            <li *ngFor="let ac of link.actions"  [class.disabled]="isDisabled(ac)" class"dropdown">
+            <li *ngFor="let ac of link.actions"  [class.disabled]="isDisabled(ac)" class="dropdown">
               <a href [class.dropdown-item]="useBootstrap4" [class.disabled]="useBootstrap4 && isDisabled(ac)"
             (click)="execute(ac, $event); $event.preventDefault(); $event.stopPropagation();"
             innerHTML="{{ac.html ? ac.html(item) : ''}}"></a>
